48b254211f871da83c5ce65e2942ec6a3378f235
rajan <rajanshahi1267@gmail.com>
2023-09-08 10:12:36 -0700

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Computer from '../pages/Computer'
+// import React from 'react'
+// import Computer from '../pages/Computer'
 // import Tablets from '../pages/Tablets'
 // import SmartPhone from '../pages/SmartPhone'
+
+import Game from "../pages/Game";
 
 const Home = () => {
   return (
     <div>
       {/* <SmartPhone/> */}
       {/* <Tablets/> */}
-      <Computer/>
+      {/* <Computer/> */}
+      <Game />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
