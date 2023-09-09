c45b6be77ae6ffd4155fce9d98a63fdf63951cd1
rajan <rajanshahi1267@gmail.com>
2023-09-08 20:51:15 -0700

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,21 @@
 // import React from 'react'
 // import Computer from '../pages/Computer'
 // import Tablets from '../pages/Tablets'
-// import SmartPhone from '../pages/SmartPhone'
+import SmartPhone from '../pages/SmartPhone'
 
 // import Accessories from "../pages/Accessories"
 // import Game from "../pages/Game"
-import Mobiles from './../pages/Mobiles';
+// import Mobiles from './../pages/Mobiles';
 
 const Home = () => {
   return (
     <div>
-      {/* <SmartPhone/> */}
+      <SmartPhone/>
       {/* <Tablets/> */}
       {/* <Computer/> */}
       {/* <Game/> */}
       {/* <Accessories/> */}
-      <Mobiles/>
+      {/* <Mobiles/> */}
     </div>
   )
 }
