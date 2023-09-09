ada1b2802fa5797112f57f1f2a2d320609583f24
rajan <rajanshahi1267@gmail.com>
2023-09-09 01:25:29 -0700

    add

--- src/components/RecentProducr.jsx
+++ src/components/RecentProducr.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation } from "swiper/modules";
 const RecentProducr = () => {
   return (
     <div className=" flex justify-center">
-      <div className=" flex w-10/12 py-8">
+      <div className=" flex w-10/12 py-8  flex-col gap-8">
         <div className=" text-gray-800 p-4 border-b-2 w-full">
           <h1 className=" text-2xl "> Recent Products</h1>
+        </div>
+        <div className=" flex justify-center items-center px-12">
+          <Swiper
+            slidesPerView={5}
+            spaceBetween={10}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+          
+          </Swiper>
         </div>
       </div>
     </div>
