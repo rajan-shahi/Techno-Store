d925f56f97287df38a26f3c49620906defdf3694
rajan <rajanshahi1267@gmail.com>
2023-09-10 04:01:53 -0700

    add

--- src/pages/SmartPhone.jsx
+++ src/pages/SmartPhone.jsx
@@ -15,7 +15,7 @@ const SmartPhone = () => {
       <div className=" w-full md:w-10/12  bg-gray-100 py-5 px-10">
         {/* top */}
         <div className=" grid grid-cols-2 gap-4 md:gap-1 md:flex justify-between text-gray-400  border-b-2 pb-2 ">
-          <h1 
+          <h1
             onClick={() => setLists(smartphones)}
             className="  px-4   text-sm md:text-2xl pb-3 md:hover:text-white/80 hover:text-orange-500 md:hover:bg-gray-600 md:hover:rounded-full hover:text-white/80  cursor-pointer"
           >
