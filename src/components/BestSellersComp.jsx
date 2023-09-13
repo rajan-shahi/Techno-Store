622e63e18d4ada3e1f164526ce71cdd54eca442b
rajan <rajanshahi1267@gmail.com>
2023-09-13 06:18:24 -0700

    add

--- src/components/BestSellersComp.jsx
+++ src/components/BestSellersComp.jsx
@@ -1,19 +1,42 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 const BestSellersComp = () => {
   return (
     <div className=" flex justify-center">
-      <div className=" flex flex-col w-10/12">
+      <div className=" flex flex-col  w-10/12">
         <div className=" flex  text-2xl text-gray-500 border-b-2 w-full py-3">
           Best Sellers
         </div>
-        <div className=" flex flex-col py-2 ">
-          <div>
-            <img src="" alt="" />
-          </div>
-          <div>
-            <h1>name</h1>
-          </div>
+        <div className=" flex justify-center py-4">
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
