84d86144438b30dbd12179fb50ef932ee5646943
rajan <rajanshahi1267@gmail.com>
2023-09-13 06:28:11 -0700

    add

--- src/components/BestSellersComp.jsx
+++ src/components/BestSellersComp.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import One from "../assets/01.jpg";
+import Two from "../assets/02.jpg";
+import Five from "../assets/05.jpg";
+import Six from "../assets/06.jpg";
+import Three from "../assets/03.jpg";
+import Four from "../assets/04.jpg";
+import Seven from "../assets/07.jpg";
+import Eighteen from "../assets/18.jpg";
+import Seventeen from "../assets/17.jpg";
+import Ninteen from "../assets/19.jpg";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -27,15 +37,21 @@ const BestSellersComp = () => {
             modules={[Autoplay, Pagination, Navigation]}
             className="mySwiper"
           >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
+            {bestsellers.map((bestseller, index) => (
+              <div key={ index}>
+                <SwiperSlide>
+                  <div>
+                    <div>
+                      <img src={bestseller.image} alt="" />
+                    </div>
+                    <div>
+                      <h1>{bestseller.name}</h1>
+                    </div>
+                  </div>
+                </SwiperSlide>
+               
+              </div>
+            ))}
           </Swiper>
         </div>
       </div>
@@ -44,3 +60,102 @@ const BestSellersComp = () => {
 };
 
 export default BestSellersComp;
+
+const bestsellers = [
+  {
+    image: One,
+    name: "Laptop",
+    model: "Apple ipad Mini",
+    desc: "G2356",
+    price: " $ 1250.00",
+    card: "Add to Card",
+  },
+  {
+    image: Two,
+    name: "Cameras",
+    model: "New X5c-12.4Ghz Gyro",
+    desc: "Rc Quadcopter Drone",
+    price: " $ 12590.00",
+    card: "Add to Card",
+  },
+  {
+    image: Five,
+    name: "Cameras",
+    model: "Apple Ipad New",
+    desc: "G2356",
+    price: " $ 12400.00",
+    card: "Add to Card",
+  },
+  {
+    image: Six,
+    name: "Cameras",
+    model: "New X5c-12563Djk",
+    desc: "CKli-01Jb",
+    price: " $ 12542.00",
+    card: "Add to Card",
+  },
+  {
+    image: Three,
+    name: "Headphones",
+    model: "Beats Solo",
+    desc: "HD",
+    price: " $ 756250.00",
+    card: "Add to Card",
+  },
+  {
+    image: Seventeen,
+    name: "Computers",
+    model: "Beats snarkitecture",
+    desc: "Headphones",
+    price: " $ 12450.00",
+    card: "Add to Card",
+  },
+  {
+    image: Four,
+    name: "Computers",
+    model: "Apple Imac Zosc4824",
+    desc: "Retin",
+    price: " $ 12850.00",
+    card: "Add to Card",
+  },
+  {
+    image: Seven,
+    name: "Computer",
+    model: "Apple ipad 1232BG 9.7",
+    desc: "Tablet",
+    price: " $ 42250.00",
+    card: "Add to Card",
+  },
+  {
+    image: Eighteen,
+    name: "Computers",
+    model: "smartphone 75",
+    desc: "123GB",
+    price: " $ 12552.03650",
+    card: "Add to Card",
+  },
+  {
+    image: Seventeen,
+    name: "Computers",
+    model: "Beats snarkitecture",
+    desc: "Headphones",
+    price: " $ 12450.00",
+    card: "Add to Card",
+  },
+  {
+    image: Ninteen,
+    name: "Computer",
+    model: "Notebook Widescreen Z51-120",
+    desc: "40K6013UPB10",
+    price: " $ 12050.010",
+    card: "Add to Card",
+  },
+  {
+    image: Three,
+    name: "Headphones",
+    model: "Beats Solo",
+    desc: "HD",
+    price: " $ 756250.00",
+    card: "Add to Card",
+  },
+];
