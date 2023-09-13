f4e40a9c7ab47b9a020c52d6a6f4312986e92893
rajan <rajanshahi1267@gmail.com>
2023-09-13 06:04:13 -0700

    add

--- src/components/BestSellersComp.jsx
+++ src/components/BestSellersComp.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const BestSellersComp = () => {
   return (
-    <div>
-      this is a best sellers compn
+    <div className=" flex justify-center">
+      <div className=" flex flex-col w-10/12">
+        <div className=" flex  text-2xl text-gray-500 border-b-2 w-full py-3">
+          Best Sellers
+        </div>
+        <div className=" flex flex-col py-2 ">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h1>name</h1>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BestSellersComp
+export default BestSellersComp;
