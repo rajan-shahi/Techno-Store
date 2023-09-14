58169b23395c90e6ba636d3c846cd3bfa46f9758
rajan <rajanshahi1267@gmail.com>
2023-09-14 03:05:19 -0700

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div className=" flex justify-center">
-      <div className=" flex  w-full md:w-10/12 md:py-8  py-4 justify-center bg-gray-300">
+      <div className=" flex  w-full md:py-8  py-4 justify-center bg-gray-300">
         <div className=" flex flex-col  shadow-md shadow-slate-400 md:px-8  px-4 md:py-8 w-[400px]">
           <span className=" text-xl text-white py-1 cursor-pointer">First Name:</span>
           <input name="name"
@@ -33,7 +33,7 @@ const Contact = () => {
             rows="10"
           ></textarea> 
           <div className=" py-4 text-right">
-          <button className=" text-xl text-white bg-green-400 hover:bg-orange-400 w-max px-4 py-2 rounded-full cursor-pointer ">Submit</button>
+          <button className=" text-xl text-white bg-green-400 hover:bg-orange-400 w-max px-6 py-2 rounded-full cursor-pointer ">Submit</button>
           </div>
         </div>
       </div>
