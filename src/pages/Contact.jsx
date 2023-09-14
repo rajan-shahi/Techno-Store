36e481ab01e5c3ee817215433f009a4eeda0b9fb
rajan <rajanshahi1267@gmail.com>
2023-09-14 01:34:25 -0700

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,18 +3,19 @@ import React from "react";
 const Contact = () => {
   return (
     <div className=" flex justify-center">
-      <div className=" flex w-10/12 justify-center bg-gray-300 text-white">
-        <div className=" flex flex-col gap-2 shadow-md shadow-slate-400 px-8 py-8">
-          First Name:
+      <div className=" flex w-10/12 py-8 justify-center bg-gray-300">
+        <div className=" flex flex-col  shadow-md shadow-slate-400 px-8 py-8 w-[400px]">
+           <span className=" text-2xl text-white py-1">First Name:</span>
           <input
-            className=""
+            className="  p-2 rounded-full cursor-pointer"
             type="text"
             placeholder=" enter your first name........."
           />
-          Last Name:
-          <input className="" type="text" placeholder=" enter your last name ........" />
-          Subject:
+          <span className=" text-2xl  text-white py-1">Last Name:</span>
+          <input className=" p-2 rounded-full" type="text" placeholder=" enter your last name ........" />
+           <span>Subject:</span>
           <input className="" type="text" placeholder="subject ............" />
+         <textarea className=" h-40" name=""  placeholder=" message ......" id="" cols="10" rows="10"></textarea>
         </div>
       </div>
     </div>
