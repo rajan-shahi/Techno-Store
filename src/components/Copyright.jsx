8e11a70e1c65f56a70dc5992c721b903f8d3a435
rajan <rajanshahi1267@gmail.com>
2023-09-14 01:06:05 -0700

    add

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -5,12 +5,12 @@ const Copyright = () => {
   return (
     <div className=" flex justify-center">
       <div className=" justify-between flex w-full md:w-10/12  pb-6 px-8 text-sm sm:text-md">
-        <div>
+        <div className=" cursor-pointer">
           <p>All Rights Reserved © Rajan Bahadur Shahi 2023</p>
         </div>
         <div>
           <Link to="navbar" spy={true} smooth={true} duration={500}>
-            <AiOutlineArrowUp size={30} />
+            <AiOutlineArrowUp size={30}  className=" cursor-pointer"/>
           </Link>
         </div>
       </div>
