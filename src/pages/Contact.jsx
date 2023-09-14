83e2135ecab20937ae20ba24377d824d265579e6
rajan <rajanshahi1267@gmail.com>
2023-09-14 01:59:06 -0700

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,27 +5,27 @@ const Contact = () => {
     <div className=" flex justify-center">
       <div className=" flex w-10/12 py-8 justify-center bg-gray-300">
         <div className=" flex flex-col  shadow-md shadow-slate-400 px-8 py-8 w-[400px]">
-          <span className=" text-2xl text-white py-1 cursor-pointer">First Name:</span>
+          <span className=" text-xl text-white py-1 cursor-pointer">First Name:</span>
           <input
-            className="  p-2 rounded-full bg-gray-100 "
+            className="  p-2 rounded-full bg-gray-100 outline-orange-400 "
             type="text"
             placeholder=" Enter your first name........."
           />
-          <span className=" text-2xl  text-white py-1 cursor-pointer">Last Name:</span>
+          <span className=" text-xl  text-white py-1 cursor-pointer">Last Name:</span>
           <input
-            className=" p-2 rounded-full"
+            className=" p-2 rounded-full outline-orange-400 "
             type="text"
             placeholder=" Enter your last name ........"
           />
-          <span className=" text-2xl  text-white p-1 cursor-pointer">Subject:</span>
+          <span className=" text-xl  text-white p-1 cursor-pointer">Subject:</span>
           <input
-            className=" p-2 rounded-full "
+            className=" p-2 rounded-full outline-orange-400  "
             type="text"
             placeholder="subject ............"
           />
-          <span className=" text-2xl text-white py-1 cursor-pointer">Message:</span>
+          <span className=" text-xl text-white py-1 cursor-pointer">Message:</span>
           <textarea
-            className=" h-40 rounded-2xl py-1"
+            className=" h-40 rounded-2xl py-1 outline-orange-400 "
             placeholder=" message................"
             name=""
             id=""
@@ -33,7 +33,7 @@ const Contact = () => {
             rows="10"
           ></textarea>
           <div className=" py-4 text-right">
-          <button className=" text-2xl text-white bg-green-400 hover:bg-orange-400 w-max px-4 py-2 rounded-full cursor-pointer ">Submit</button>
+          <button className=" text-xl text-white bg-green-400 hover:bg-orange-400 w-max px-4 py-2 rounded-full cursor-pointer ">Submit</button>
           </div>
         </div>
       </div>
