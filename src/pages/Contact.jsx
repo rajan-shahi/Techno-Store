cba1a4713312a387b3ab4828043b0e114cd359c8
rajan <rajanshahi1267@gmail.com>
2023-09-14 01:48:03 -0700

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,17 +5,36 @@ const Contact = () => {
     <div className=" flex justify-center">
       <div className=" flex w-10/12 py-8 justify-center bg-gray-300">
         <div className=" flex flex-col  shadow-md shadow-slate-400 px-8 py-8 w-[400px]">
-           <span className=" text-2xl text-white py-1">First Name:</span>
+          <span className=" text-2xl text-white py-1">First Name:</span>
           <input
             className="  p-2 rounded-full cursor-pointer"
             type="text"
-            placeholder=" enter your first name........."
+            placeholder=" Enter your first name........."
           />
           <span className=" text-2xl  text-white py-1">Last Name:</span>
-          <input className=" p-2 rounded-full" type="text" placeholder=" enter your last name ........" />
-           <span>Subject:</span>
-          <input className="" type="text" placeholder="subject ............" />
-         <textarea className=" h-40" name=""  placeholder=" message ......" id="" cols="10" rows="10"></textarea>
+          <input
+            className=" p-2 rounded-full"
+            type="text"
+            placeholder=" Enter your last name ........"
+          />
+          <span className=" text-2xl  text-white p-1">Subject:</span>
+          <input
+            className=" p-2 rounded-full cursor-pointer"
+            type="text"
+            placeholder="subject ............"
+          />
+          <span className=" text-2xl text-white py-1">Message:</span>
+          <textarea
+            className=" h-40 rounded-2xl"
+            placeholder=" message................"
+            name=""
+            id=""
+            cols="10"
+            rows="10"
+          ></textarea>
+          <div className=" py-4 text-right">
+          <button className=" text-2xl text-white bg-green-400 hover:bg-orange-400 w-max px-4 py-2 rounded-full cursor-pointer ">Submit</button>
+          </div>
         </div>
       </div>
     </div>
