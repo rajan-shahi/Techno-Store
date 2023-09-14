2fa64a66c0e35abc8a5063fbc635d6361415b922
rajan <rajanshahi1267@gmail.com>
2023-09-14 01:21:59 -0700

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      contact
+    <div className=" flex justify-center">
+      <div className=" flex w-10/12 justify-center bg-gray-300 text-white">
+        <div className=" flex flex-col gap-2 shadow-md shadow-slate-400 px-8 py-8">
+          First Name:
+          <input
+            className=""
+            type="text"
+            placeholder=" enter your first name........."
+          />
+          Last Name:
+          <input className="" type="text" placeholder=" enter your last name ........" />
+          Subject:
+          <input className="" type="text" placeholder="subject ............" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
