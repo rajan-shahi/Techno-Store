16933d0aebbff5daddfffab740b52c8f993c96ff
rajan <rajanshahi1267@gmail.com>
2023-09-14 01:52:55 -0700

    add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,27 +5,27 @@ const Contact = () => {
     <div className=" flex justify-center">
       <div className=" flex w-10/12 py-8 justify-center bg-gray-300">
         <div className=" flex flex-col  shadow-md shadow-slate-400 px-8 py-8 w-[400px]">
-          <span className=" text-2xl text-white py-1">First Name:</span>
+          <span className=" text-2xl text-white py-1 cursor-pointer">First Name:</span>
           <input
-            className="  p-2 rounded-full cursor-pointer"
+            className="  p-2 rounded-full bg-gray-100 "
             type="text"
             placeholder=" Enter your first name........."
           />
-          <span className=" text-2xl  text-white py-1">Last Name:</span>
+          <span className=" text-2xl  text-white py-1 cursor-pointer">Last Name:</span>
           <input
             className=" p-2 rounded-full"
             type="text"
             placeholder=" Enter your last name ........"
           />
-          <span className=" text-2xl  text-white p-1">Subject:</span>
+          <span className=" text-2xl  text-white p-1 cursor-pointer">Subject:</span>
           <input
-            className=" p-2 rounded-full cursor-pointer"
+            className=" p-2 rounded-full "
             type="text"
             placeholder="subject ............"
           />
-          <span className=" text-2xl text-white py-1">Message:</span>
+          <span className=" text-2xl text-white py-1 cursor-pointer">Message:</span>
           <textarea
-            className=" h-40 rounded-2xl"
+            className=" h-40 rounded-2xl py-1"
             placeholder=" message................"
             name=""
             id=""
