c9670ec5f036a742cf54fcf4b3cf16a655c3dd15
rajan <rajanshahi1267@gmail.com>
2023-09-15 05:38:21 -0700

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
       <div className=" flex  w-full md:py-8  py-4 justify-center ">
         <div className=" flex flex-col  shadow-md shadow-slate-400 md:px-8 bg-gray-200  px-4 md:py-8 w-[400px]">
           <span className=" text-gray-700   font-bold py-1 cursor-pointer">
-            First Name:
+            First Name*
           </span>
           <input
             name="name"
@@ -12,17 +12,17 @@ const Contact = () => {
             type="text"
             placeholder=" Enter your first name........."
           />
-          <span className=" font-bold text-white py-1 cursor-pointer">
-            Last Name:
+          <span className=" font-bold  text-gray-700 py-1 cursor-pointer">
+            Last Name*
           </span>
           <input
             name="name"
-            className=" p-2 rounded-full outline-orange-400 px-2  font-bold"
+            className=" p-2 rounded-full outline-orange-400 px-2 "
             type="text"
             placeholder=" Enter your last name ........"
           />
-          <span className=" text-xl  text-white p-1 cursor-pointer">
-            Subject:
+          <span className=" font-bold   text-gray-700 p-1 cursor-pointer">
+            Subject
           </span>
           <input
             name="subject"
@@ -30,8 +30,8 @@ const Contact = () => {
             type="text"
             placeholder=" subject ............"
           />
-          <span className=" text-xl text-white py-1 cursor-pointer">
-            Message:
+          <span className=" font-bold text-gray-700 py-1 cursor-pointer">
+            Message
           </span>
           <textarea
             className=" h-40 rounded-2xl py-1 outline-orange-400 px-2 "
@@ -42,7 +42,7 @@ const Contact = () => {
             rows="10"
           ></textarea>
           <div className=" py-4 text-right">
-            <button className=" text-xl text-white bg-green-400 hover:bg-orange-400 w-max px-6 py-2 rounded-full cursor-pointer ">
+            <button className="   text-gray-700  font-bold bg-green-400 hover:bg-orange-400 w-max px-6 py-2 rounded-full cursor-pointer ">
               Submit
             </button>
           </div>
