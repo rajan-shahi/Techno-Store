d9b50edf92199f2c8f9cf7064fa77fcb61183867
rajan <rajanshahi1267@gmail.com>
2023-09-15 09:14:33 -0700

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,9 @@
 const Contact = () => {
   return (
     <div className=" flex justify-center">
-      <div className=" flex  w-full md:py-8  py-4 justify-center ">
+      <div className=" flex  w-full md:py-8 py-0    justify-center ">
         <div className=" flex flex-col  shadow-md shadow-slate-400 md:px-8 bg-gray-200  px-4 md:py-8 w-[400px]">
-          <span className=" text-gray-700   font-bold  p-1 cursor-pointer">
+          <span className=" text-gray-700   font-bold md:py-0 pt-8  p-1 cursor-pointer">
             First Name*
           </span>
           <input
@@ -41,7 +41,7 @@ const Contact = () => {
             cols="10"
             rows="10"
           ></textarea>
-          <div className=" py-4 text-right">
+          <div className=" pt-6 md:pb-0 pb-6 text-right">
             <button className="   text-gray-700  font-bold bg-green-400 hover:bg-orange-400 w-max px-6 py-2 rounded-full cursor-pointer ">
               Submit
             </button>
