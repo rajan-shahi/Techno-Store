604d3743d66f7206cd293a92ceea859667093785
rajan <rajanshahi1267@gmail.com>
2023-09-15 05:22:09 -0700

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,38 @@
-import React from "react";
-
 const Contact = () => {
   return (
     <div className=" flex justify-center">
-      <div className=" flex  w-full md:py-8  py-4 justify-center bg-gray-300">
-        <div className=" flex flex-col  shadow-md shadow-slate-400 md:px-8  px-4 md:py-8 w-[400px]">
-          <span className=" text-xl text-white py-1 cursor-pointer">First Name:</span>
-          <input name="name"
+      <div className=" flex  w-full md:py-8  py-4 justify-center ">
+        <div className=" flex flex-col  shadow-md shadow-slate-400 md:px-8 bg-gray-200  px-4 md:py-8 w-[400px]">
+          <span className=" text-gray-700   font-bold py-1 cursor-pointer">
+            First Name:
+          </span>
+          <input
+            name="name"
             className="  p-2 rounded-full bg-gray-100 outline-orange-400 px-2 "
             type="text"
             placeholder=" Enter your first name........."
           />
-          <span className=" text-xl  text-white py-1 cursor-pointer">Last Name:</span>
-          <input name="name"
-            className=" p-2 rounded-full outline-orange-400 px-2 "
+          <span className=" font-bold text-white py-1 cursor-pointer">
+            Last Name:
+          </span>
+          <input
+            name="name"
+            className=" p-2 rounded-full outline-orange-400 px-2  font-bold"
             type="text"
             placeholder=" Enter your last name ........"
           />
-          <span className=" text-xl  text-white p-1 cursor-pointer">Subject:</span>
-          <input name="subject"
+          <span className=" text-xl  text-white p-1 cursor-pointer">
+            Subject:
+          </span>
+          <input
+            name="subject"
             className=" p-2 rounded-full outline-orange-400 px-2  "
             type="text"
             placeholder=" subject ............"
           />
-          <span className=" text-xl text-white py-1 cursor-pointer">Message:</span>
+          <span className=" text-xl text-white py-1 cursor-pointer">
+            Message:
+          </span>
           <textarea
             className=" h-40 rounded-2xl py-1 outline-orange-400 px-2 "
             placeholder=" message................"
@@ -31,9 +40,11 @@ const Contact = () => {
             id=""
             cols="10"
             rows="10"
-          ></textarea> 
+          ></textarea>
           <div className=" py-4 text-right">
-          <button className=" text-xl text-white bg-green-400 hover:bg-orange-400 w-max px-6 py-2 rounded-full cursor-pointer ">Submit</button>
+            <button className=" text-xl text-white bg-green-400 hover:bg-orange-400 w-max px-6 py-2 rounded-full cursor-pointer ">
+              Submit
+            </button>
           </div>
         </div>
       </div>
