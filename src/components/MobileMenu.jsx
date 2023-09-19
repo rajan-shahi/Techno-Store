880232bd8c1c45550333ea4b9a6e89a58f03f835
rajan <rajanshahi1267@gmail.com>
2023-09-19 01:52:30 -0700

    add

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { AiOutlineMenu } from "react-icons/ai";
 import { AiOutlineClose } from "react-icons/ai";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
 const MobileMenu = () => {
   const [nav, setNev] = useState(false);
   const handleClick = () => setNev(!nav);
   return (
-    <div  className="  md:hidden flex justify-center ">
+    <div className="  md:hidden flex justify-center ">
       <div className=" w-full  md:w-10/12 py-4   ">
         <div className=" flex items-center justify-between cursor-pointer ">
           <img src={logo} alt="" className=" px-4" />
@@ -27,32 +27,25 @@ const MobileMenu = () => {
                 : "   md:z-0 z-40  absolute   w-full py-6   flex flex-col  justify-center   bg-gray-500 items-center rounded-md   "
             }
           >
-           
-        
             <li className="  hover:text-orange-400 cursor-pointer py-4 text-4xl  text-gray-200">
-            <Link to="Home" spy={true} smooth={true} duration={500}>Home</Link>
+              <Link to={"/about"}>
+              About
+              </Link>
             </li>
-            
+
             <li className="   hover:text-orange-400 cursor-pointer py-4 text-4xl text-gray-200">
-            <Link to="NewArrivals" spy={true} smooth={true} duration={500}>  New Arrivals</Link>  
+              {" "}
+              <Link to={"/shopbar"}>
+              Shop cards
+              </Link>
+              
             </li>
             <li className="   hover:text-orange-400  cursor-pointer py-4 text-4xl  text-gray-200">
-            <Link to="featured" spy={true} smooth={true} duration={500}> Features</Link>
-            </li>
-            <li className="   hover:text-orange-400 cursor-pointer py-4 text-4xl  text-gray-200">
-            <Link to="smartPhone" spy={true} smooth={true} duration={500}> SmartPhones</Link> 
-            </li>
-            <li className="   hover:text-orange-400 cursor-pointer py-4 text-4xl  text-gray-200">
-            <Link to="product" spy={true} smooth={true} duration={500}>  Recent Products</Link>
-            </li>
-            <li className="   hover:text-orange-400 cursor-pointer py-4 text-4xl  text-gray-200">
-            <Link to="seller" spy={true} smooth={true} duration={500}> Bestsellers</Link> 
-            </li>
-            <li className="   hover:text-orange-400   cursor-pointer py-4 text-4xl  text-gray-200">
-            <Link to="payment" spy={true} smooth={true} duration={500}> Payments</Link> 
-            </li>
-            <li className="   hover:text-orange-400   cursor-pointer py-4 text-4xl  text-gray-200">
-            <Link to="contact" spy={true} smooth={true} duration={500}> Contacts</Link> 
+              {" "}
+              <Link to={"/contact"}>
+              Contacts
+              </Link>
+          
             </li>
           </ul>
         </div>
