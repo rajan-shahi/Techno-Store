ef39e1fa097278bbd5ea56c6d3f4adedc23f7407
rajan <rajanshahi1267@gmail.com>
2023-09-21 05:36:59 -0700

    add new account

--- src/components/AddNeweCard.jsx
+++ src/components/AddNeweCard.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { BsArrowRight } from "react-icons/bs";
 const AddNeweCard = () => {
   return (
-    <div className=" flex justify-center py-5">
+    <div className=" flex  justify-center py-5">
       <div className=" md:flex md:px-0 px-4  justify-between  md:w-10/12 w-full">
         {/* left */}
         <div className="md:w-5/12 w-full   flex flex-col  gap-6 justify-center">
           <div className=" flex flex-col justify-center items-center">
             <h1 className=" text-3xl text-gray-500 flex justify-center cursor-pointer hover:text-orange-400">
-              <span>Add New Card</span> /or
+              <span>Add New Card</span> 
             </h1>
-            <Link to={"/"}>
-              <h2 className=" text-sm  text-gray-400 cursor-pointer hover:text-red-600">
-                you have not add to card
-              </h2>
-            </Link>
           </div>
           <div className=" flex flex-col gap-1">
             <span className=" text-sm text-gray-500">Title</span>
@@ -90,13 +85,13 @@ const AddNeweCard = () => {
             </div>
 
             <div className=" flex flex-col gap-1">
-            <span className=" text-sm text-gray-500">Add features</span>
-            <input
-              className=" border-2 px-2 py-2 md:w-[500px] w-[350px] rounded-full outline-orange-400 placeholder:text-sm"
-              type="text"
-              placeholder="e.g page design"
-            />
-          </div>
+              <span className=" text-sm text-gray-500">Add features</span>
+              <input
+                className=" border-2 px-2 py-2 md:w-[500px] w-[350px] rounded-full outline-orange-400 placeholder:text-sm"
+                type="text"
+                placeholder="e.g page design"
+              />
+            </div>
             <div className=" flex flex-col gap-1">
               <span className=" text-sm text-gray-500">Price</span>
               <input
@@ -109,6 +104,12 @@ const AddNeweCard = () => {
               <button className=" flex justify-center text-md text-white bg-blue-400 border-2  py-2 md:w-[500px] w-[350px] rounded-lg  hover:bg-orange-300  ">
                 Create
               </button>
+            </div>
+            <div className=" flex px-10  justify-end items-center">
+            <Link to={"/"}> 
+              <span className=" cursor-pointer">
+                <BsArrowRight size={25} />
+              </span>   </Link>
             </div>
           </div>
         </div>
