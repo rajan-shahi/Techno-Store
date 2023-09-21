61a4586b2ac7070eafd3cd862567477fcbefa6c9
rajan <rajanshahi1267@gmail.com>
2023-09-21 06:16:49 -0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
     <div className="  flex justify-center  border-b-2 md:pt-0 pt-16 ">
@@ -10,6 +10,57 @@ const Navbar = () => {
         className="py-3 w-10/12 flex flex-col gap-5  md:flex-row  justify-between text-xs  items-center sticky "
       >
         {/* leftbar */}
+        <div className="md:flex hidden">
+          <img className=" cursor-pointer" sizes="20" src={logo} alt="" />
+        </div>
+
+        {/* midbar */}
+        <div className=" text-gray-600">
+          <p>
+            {" "}
+            <span className="text-orange-600 ">Call Us: </span>{" "}
+            <span className=" cursor-pointer">(888) 123456789</span>
+          </p>
+        </div>
+
+        {/* mobile menu */}
+        <div className=" flex  gap-3 md:hidden items-center">
+          <select
+            id=""
+            className=" border-2  px-4 py-2 rounded-full  items-center flex text-gray-600"
+          >
+            <option value="">My Account</option>
+            <option value="Login">Login</option>
+            <option value="Wishlist">Wishlist</option>
+            <option value="My Cart">My Cart</option>
+            <option value="CheckOut">CheckOut</option>
+            <option value="MP3">MP3 </option>
+          </select>
+          <select
+            id=""
+            className=" border-2  px-4 py-2 rounded-full  items-center flex text-gray-600"
+          >
+            <option value="">USD</option>
+            <option value="USD">USD</option>
+            <option value="Euro">Euro</option>
+            <option value="Dolar">Dolar</option>
+            <option value="Nepali">Nepali</option>
+          </select>
+          <select
+            id=""
+            className=" border-2  px-4 py-2 rounded-full  items-center flex text-gray-600"
+          >
+            <option value="">Language</option>
+            <option value="English">English</option>
+            <option value="Nepali">Nepali</option>
+            <option value="Espanol">Espanol</option>
+            <option value="Italion">Italion</option>
+            <option value="Turkish">Turkish</option>
+          </select>
+        </div>
+
+        {/* righrbar */}
+
         <div className=" hidden md:flex gap-4 text-gray-600 text-xl ">
           <Link to={"/contact"}>
             <p className=" border-r-2 text-md  pr-2 hover:text-orange-600 cursor-pointer">
@@ -23,44 +74,14 @@ const Navbar = () => {
           </Link>
 
           <Link to={"/shopbar"}>
-            <p className=" border-r-2 pr-2 hover:text-orange-600 cursor-pointer">Shop Cards</p>
+            <p className=" border-r-2 pr-2 hover:text-orange-600 cursor-pointer">
+              Shop Cards
+            </p>
           </Link>
 
-          
           <Link to={"/signin"}>
             <p className="  hover:text-orange-600 cursor-pointer">Sign in</p>
           </Link>
-        </div>
-        {/* midbar */}
-        <div className=" text-gray-600">
-          <p>
-            {" "}
-            <span className="text-orange-600 ">Call Us: </span>{" "}
-            <span className=" cursor-pointer">(888) 123456789</span>
-          </p>
-        </div>
-        {/* righrbar */}
-        <div className=" flex gap-4 text-gray-600">
-          <select className=" flex flex-col  border-2  py-2 rounded-full  px-4 cursor-pointer ">
-            <option value=""> My Account</option>
-            <option value="Login">Login</option>
-            <option value="Wishlist">Wishlist</option>
-            <option value="My Cart">My cart</option>
-            <option value="My Account">My Account</option>
-            <option value="Checkout">CheckOut</option>
-          </select>
-          <select id="" className=" border-2 py-2 rounded-full  px-4 cursor-pointer">
-            <option value="">USD</option>
-            <option value="Euro">Euro</option>
-            <option value="Dolar">Dolar</option>
-          </select>
-          <select id="" className="  border-2 py-2 px-4 rounded-full cursor-pointer text-sm">
-            <option value="">Language</option>
-            <option value="Turkish">Turkish</option>
-            <option value="Englidh">English</option>
-            <option value="Espanol">Espanol</option>
-            <option value="Italion">Italion</option>
-          </select>
         </div>
       </div>
     </div>
--- src/components/NavbrFooter.jsx
+++ src/components/NavbrFooter.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "../assets/logo.png";
 import { FiSearch } from "react-icons/fi";
 import { BsArrowLeftRight } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
@@ -8,28 +7,35 @@ const NavbrFooter = () => {
   return (
     <div className=" hidden md:flex h-[120px] justify-center  items-center">
       {/* container */}
-      <div className="  w-10/12 flex justify-between ">
+      <div className="  w-10/12 flex justify-between  ">
         {/* left */}
-        <div>
-          <img className=" cursor-pointer" sizes="20" src={logo} alt="" />
-        </div>
         {/* mid */}
         <div className=" flex  w-[500px] border-2 rounded-full  px-2 text-sm">
           <div className=" flex items-center">
-            <select id="" className=" border-r-2  px-4 py-1  items-center flex text-gray-600">
+            <select
+              id=""
+              className=" border-r-2  px-4 py-1  items-center flex text-gray-600"
+            >
               <option value="">All Category</option>
-              <option  className="text-2xl" value="Electronics">Electronics</option>
+              <option className="text-2xl" value="Electronics">
+                Electronics
+              </option>
               <option value="Components">Components</option>
-              <option value="Laptop">Laptop</option> 
+              <option value="Laptop">Laptop</option>
               <option value="Monitor">Monitor</option>
               <option value="MP3">MP3 </option>
               <option value="Scanner">Scanner</option>
-              <option  className="text-2xl"value=""> Furniters</option>
+              <option className="text-2xl" value="">
+                {" "}
+                Furniters
+              </option>
               <option value="">Bookcase</option>
               <option value="">Tv stand</option>
               <option value="">Desks</option>
               <option value="">Chairs</option>
-              <option  className="text-2xl" value="">Accessories</option>
+              <option className="text-2xl" value="">
+                Accessories
+              </option>
               <option value="">Software</option>
               <option value="">Mobile</option>
               <option value="">Media</option>
@@ -52,14 +58,18 @@ const NavbrFooter = () => {
           <span className=" rounded-full px-3 py-3 border-2 hover:border-orange-300   hover:border-2 cursor-pointer">
             <BsArrowLeftRight size={30} className=" text-gray-300" />
           </span>
-          <span  className=" rounded-full px-3 py-3 border-2 hover:border-orange-300  hover:border-2 cursor-pointer">
-            <AiOutlineHeart  size={30} className=" text-gray-300"/>
+          <span className=" rounded-full px-3 py-3 border-2 hover:border-orange-300  hover:border-2 cursor-pointer">
+            <AiOutlineHeart size={30} className=" text-gray-300" />
           </span>
-          <span  className=" rounded-full px-3 py-3 border-2 hover:border-orange-300  hover:border-2 cursor-pointer">
-            <FaShoppingCart size={30} className=" text-gray-300"/>
+          <span className=" rounded-full px-3 py-3 border-2 hover:border-orange-300  hover:border-2 cursor-pointer">
+            <FaShoppingCart size={30} className=" text-gray-300" />
           </span>
-          <p className=" rounded-full px-2 py- bg-orange-600 ml-[-40px] mt-[-30px] cursor-pointer text-white">4</p>
-          <p className=" ml-[-30px] hover:text-orange-600 cursor-pointer">$0.00</p>
+          <p className=" rounded-full px-2 py- bg-orange-600 ml-[-40px] mt-[-30px] cursor-pointer text-white">
+            4
+          </p>
+          <p className=" ml-[-30px] hover:text-orange-600 cursor-pointer">
+            $0.00
+          </p>
         </div>
       </div>
     </div>
