bf3844265a868df21e9d61b7e9c3ce9d36bcaf02
rajan <rajanshahi1267@gmail.com>
2023-09-21 22:23:31 -0700

    add

--- src/components/BlogSideBar.jsx
+++ src/components/BlogSideBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
-import {AiOutlineMinus  } from "react-icons/ai";
+import { AiOutlineMinus } from "react-icons/ai";
 import about from "../assets/about.jpg";
 import post02 from "../assets/post-02.jpg";
 import post03 from "../assets/post-03.jpg";
@@ -160,49 +160,67 @@ const BlogSideBar = () => {
             </div>
             <div className=" py-5">
               <input
-                    className=" border bg-gray-200 py-2 px-2 rounded-full placeholder:text-sm outline-orange-400 w-[300px]"
+                className=" border bg-gray-200 py-2 px-2 rounded-full placeholder:text-sm outline-orange-400 w-[300px]"
                 type="text"
                 placeholder=" colors search"
               />
             </div>
-            <div className=" flex flex-col gap-4 py-8 text-sm text-gray-500 overflow-y-scroll  sticky h-72">
-              <div className=" flex gap-4">
-                <input className=" cursor-pointer" type="checkbox" />
-                <h1>Black (4)</h1>
+            <div className=" flex flex-col px-4">
+              <div className=" flex  items-center justify-between py-4 border-b border-gray-300">
+                <h1 className=" text-xl text-gray-600 ">Categories</h1>
+                <span className=" cursor-pointer">
+                  <AiOutlineMinus size={20} />
+                </span>
               </div>
-              <div className=" flex gap-4">
-                <input type="checkbox" />
-                <h1>Yellow (2)</h1>
-              </div>
-              <div className=" flex gap-4">
-                <input type="checkbox" />
-                <h1>White (2)</h1>
-              </div>
-              <div className=" flex gap-4">
-                <input type="checkbox" />
-                <h1>Blue (2)</h1>
-              </div>
-              <div className=" flex gap-4">
-                <input type="checkbox" />
-                <h1>Red (1)</h1>
-              </div>
-              <div className=" flex gap-4">
-                <input type="checkbox" />
-                <h1>Pink (3)</h1>
-              </div>
-              <div className=" flex gap-4">
-                <input type="checkbox" />
-                <h1>Green (4)</h1>
-              </div>
-              <div className=" flex gap-4">
-                <input type="checkbox" />
-                <h1>Gold (4)</h1>
+              <div className=" flex flex-col gap-4 py-8 text-sm text-gray-500">
+                <h1 className=" hover:text-orange-400 cursor-pointer">
+                  Accessories (03)
+                </h1>
+                <h1 className=" hover:text-orange-400 cursor-pointer">
+                  Cameras (19)
+                </h1>
+                <h1 className=" hover:text-orange-400 cursor-pointer">
+                  Computers (56)
+                </h1>
+                <h1 className=" hover:text-orange-400 cursor-pointer">
+                  Laptops (03)
+                </h1>
+                <h1 className=" hover:text-orange-400 cursor-pointer">
+                  Networking (03)
+                </h1>
+                <h1 className=" hover:text-orange-400 cursor-pointer">
+                  Old Products (89)
+                </h1>
+                <h1 className=" hover:text-orange-400 cursor-pointer">
+                  Smartphones (90)
+                </h1>
+                <h1 className=" hover:text-orange-400 cursor-pointer">
+                  Software (23)
+                </h1>
               </div>
             </div>
             <div>
-                <div>
-                <h1>Latest Products</h1></div>
-                <div></div>
+              <h1>Latest Products</h1>
+            </div>
+            <div className=" grid grid-cols-2">
+              <div>
+                <h1>SmartPhone</h1>
+              </div>
+              <div>
+                <h1>Cameras</h1>
+              </div>
+              <div>
+                <h2>HeadPhones</h2>
+              </div>
+              <div>
+                <h1>Musics</h1>
+              </div>
+              <div>
+                <h1>Computers</h1>
+              </div>
+              <div>
+                <h2>Laptops</h2>
+              </div>
             </div>
           </div>
         </div>
