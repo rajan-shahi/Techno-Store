bb3c90cbc1677a78e42244160616b19dc35aeb16
rajan <rajanshahi1267@gmail.com>
2023-10-01 10:04:57 -0700

    change the footer color

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -19,9 +19,6 @@ const MobileMenu = () => {
             )}
           </div>
         </div>
-        loki
-        rajan
-        git
         <div className=" ">
           <ul
             className={
