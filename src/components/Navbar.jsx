8a48e07860b3401dd738615e4048f28e4a3722c1
rajan <rajanshahi1267@gmail.com>
2023-12-05 03:13:37 -0700

    Addd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,21 @@ const Navbar = () => {
   return (
     <div className="  z-30 bg-white md:fixed md:left-0 md:right-0   flex justify-center  border-b-2 md:pt-4 pt-14 pb-4 ">
       {/* container */}
-      <div
-      
-        className="py-3 w-10/12 flex flex-col gap-5  md:flex-row  justify-between text-xs  items-center sticky "
-      >
+      <div className="py-3 w-10/12 flex flex-col gap-5  md:flex-row  justify-between text-xs  items-center sticky ">
         {/* leftbar */}
         <div className="md:flex hidden">
           <img className=" cursor-pointer" sizes="20" src={logo} alt="" />
         </div>
-
         {/* midbar */}
         <div className=" text-gray-600">
           <p>
             {" "}
-            <span className= "text-orange-600   text-sm md:text-xl ">Call Us: </span>{" "}
-            <span className=" cursor-pointer text-sm md:text-lg">(+977) 9866448106</span>
+            <span className="text-orange-600   text-sm md:text-xl ">
+              Call Us:{" "}
+            </span>{" "}
+            <span className=" cursor-pointer text-sm md:text-lg">
+              (+977) 9866448106
+            </span>
           </p>
         </div>
 
@@ -51,7 +51,8 @@ const Navbar = () => {
           >
             <option value="">Language</option>
             <option value="English">English</option>
-            <option value="Hindi">Hindi</option>            <option value="Nepali">Nepali</option>
+            <option value="Hindi">Hindi</option>{" "}
+            <option value="Nepali">Nepali</option>
             <option value="Espanol">Espanol</option>
             <option value="Italion">Italion</option>
             <option value="Turkish">Turkish</option>
@@ -59,11 +60,10 @@ const Navbar = () => {
         </div>
 
         {/* righrbar */}
-
         <div className=" hidden md:flex gap-4 text-gray-600 text-xl ">
-          <Link to={"/contact"}>
-            <p className=" border-r-2 text-md  pr-2 hover:text-orange-600 cursor-pointer">
-              Contact
+        <Link to={"/"}>
+            <p className=" border-r-2 pr-2  hover:text-orange-600 cursor-pointer">
+              Home
             </p>
           </Link>
           <Link to={"/about"}>
@@ -77,13 +77,18 @@ const Navbar = () => {
               Shop Cards
             </p>
           </Link>
-          
+
           <Link to={"/blog"}>
             <p className=" border-r-2 pr-2 hover:text-orange-600 cursor-pointer">
               Blog SideBar
             </p>
           </Link>
 
+          <Link to={"/contact"}>
+            <p className=" border-r-2 text-md  pr-2 hover:text-orange-600 cursor-pointer">
+              Contact
+            </p>
+          </Link>
           <Link to={"/signin"}>
             <p className="  hover:text-orange-600 cursor-pointer">Sign in</p>
           </Link>
