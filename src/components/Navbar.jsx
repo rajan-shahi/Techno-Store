e24f0d44ac6adc23a3d15ed869c2f93281e3fe34
rajan <rajanshahi1267@gmail.com>
2023-12-08 19:44:28 -0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,10 @@
-
-import { Link} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 import MobileMenu from "../components/MobileMenu";
-import { useState } from "react";
 
 const Navbar = () => {
-  const [Nav,setNav] =useState("Home");
-
-
+  const location = useLocation();
+  console.log(location.pathname);
 
   return (
     <div className=" z-30 bg-white md:fixed md:left-0 md:right-0   flex justify-center   md:border-b md:pt-4 pt-14  md:pb-4 md:py-0  py-2">
@@ -21,43 +18,48 @@ const Navbar = () => {
         {/* rightbar */}
         <div className=" hidden md:flex gap-8 text-gray-600 text-sm  font-bold">
           <Link to={"/"}>
-            <p onClick={()=>{setNav("Home");}}
-            className="  hover:text-orange-600 cursor-pointer">
-           Home {Nav ==="Home" ? <hr/>: <></>}
+            <p
+              className={`${
+                location.pathname === "/" ? "text-orange-600 " : ""
+              } cursor-pointer`}
+            >
+              Home
             </p>
           </Link>
           <Link to={"/about"}>
-          <p onClick={()=>{setNav("About");}}
-            className="  hover:text-orange-600 cursor-pointer">
-           About {Nav ==="About" ? <hr/>: <></>}
+            <p
+              className={`${
+                location.pathname === "/about" ? "text-orange-600" : ""
+              } cursor-pointer`}
+            >
+              About
             </p>
           </Link>
 
           <Link to={"/shopbar"}>
-          <p onClick={()=>{setNav("Shop Cards");}}
-            className="  hover:text-orange-600 cursor-pointer">
-           Shop Cards {Nav ==="Shop Cards" ? <hr/>: <></>}
-            </p>
+            <p  className={`${
+                location.pathname === "/shopbar" ? "text-orange-600" : ""
+              } cursor-pointer`}>
+              Shop Cards</p>
           </Link>
 
           <Link to={"/blog"}>
-          <p onClick={()=>{setNav("Blog SideBar");}}
-            className="  hover:text-orange-600 cursor-pointer">
-           Blog SideBar {Nav ==="Blog SideBar" ? <hr/>: <></>}
+            <p  className={`${
+                location.pathname === "/blog" ? "text-orange-600" : ""
+              } cursor-pointer`}>
+              Blog SideBar
             </p>
           </Link>
 
           <Link to={"/contact"}>
-          <p onClick={()=>{setNav("Contact");}}
-            className="  hover:text-orange-600 cursor-pointer">
-           Contact {Nav ==="Contact" ? <hr/>: <> </>}
-            </p>
+            <p  className={`${
+                location.pathname === "/contact" ? "text-orange-600" : ""
+              } cursor-pointer`}>Contact</p>
           </Link>
           <Link to={"/signin"}>
-          <p onClick={()=>{setNav("Login");}}
-            className="  hover:text-orange-600 cursor-pointer">
-           Login  {Nav ==="Login" ? <hr/>: <></>}
-            </p>
+            <p  className={`${
+                location.pathname === "/signin" ? "text-orange-600" : ""
+              } cursor-pointer`}>Login</p>
           </Link>
         </div>
       </div>
